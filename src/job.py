0a5d7088ce77c072639d4088590bbca75d015d4a
Nishit Grover <grovernt@mail.uc.edu>
2024-09-29 21:37:09 -0400

    Refactor job.py and strings.py files

--- src/job.py
+++ src/job.py
@@ -1,11 +1,8 @@
 from dataclasses import dataclass
 
-<<<<<<< HEAD
-=======
 from loguru import logger
 
 
->>>>>>> e1d1ea8534f538bd20053e26f4f379adeef0eca4
 @dataclass
 class Job:
     title: str
@@ -19,14 +16,6 @@ class Job:
     recruiter_link: str = ""
 
     def set_summarize_job_description(self, summarize_job_description):
-<<<<<<< HEAD
-        self.summarize_job_description = summarize_job_description
-
-    def set_job_description(self, description):
-        self.description = description
-
-    def set_recruiter_link(self, recruiter_link):
-=======
         logger.debug(f"Setting summarized job description: {summarize_job_description}")
         self.summarize_job_description = summarize_job_description
 
@@ -36,17 +25,13 @@ class Job:
 
     def set_recruiter_link(self, recruiter_link):
         logger.debug(f"Setting recruiter link: {recruiter_link}")
->>>>>>> e1d1ea8534f538bd20053e26f4f379adeef0eca4
         self.recruiter_link = recruiter_link
 
     def formatted_job_information(self):
         """
         Formats the job information as a markdown string.
         """
-<<<<<<< HEAD
-=======
         logger.debug(f"Formatting job information for job: {self.title} at {self.company}")
->>>>>>> e1d1ea8534f538bd20053e26f4f379adeef0eca4
         job_information = f"""
         # Job Description
         ## Job Information 
@@ -58,10 +43,6 @@ class Job:
         ## Description
         {self.description or 'No description provided.'}
         """
-<<<<<<< HEAD
-        return job_information.strip()
-=======
         formatted_information = job_information.strip()
         logger.debug(f"Formatted job information: {formatted_information}")
         return formatted_information
->>>>>>> e1d1ea8534f538bd20053e26f4f379adeef0eca4
